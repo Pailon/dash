1054327e3506418c49515b1d3466e15cc28e1ea8
Igor Stepanenko <temonavto997@yandex.ru>
2020-07-12 13:33:24 +0300

    new loader

--- src/components/UI/Loader/Loader.js
+++ src/components/UI/Loader/Loader.js
@@ -1,8 +1,118 @@
 import React from 'react'
 import './Loader.css'
 
+// export default () =>(
+//      <div className="loader-wrap">
+//      <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+//      </div>
+// )
+
 export default () =>(
-     <div className="loader-wrap">
-     <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-     </div>
+    <div className='container' style={{justifyContent: 'center', paddingTop:'20%', display: 'flex'}}>
+         <div className="loadingio-spinner-spinner-j0ya2895w6n">
+              <div className="ldio-8jfgaq0uopi">
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                   <div></div>
+              </div>
+         </div>
+    </div>
+
 )
